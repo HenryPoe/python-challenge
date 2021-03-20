18212669aa86eb9e4663841b321f9b1b0216e365
Henry Poe <hpoe111@gmail.com>
2021-03-20 13:36:51 -0400

    started tracking changes and their corresponding month in 2d array

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,12 +12,9 @@ from statistics import mean
 # Create the path for our data set
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-# Initialize some variable with their starting values
+# Initialize some variable that need starting values
 numMonths = 0
-total = 0
-#previousAmount = 0
-bigProfit = 0
-bigLoss = 0
+netProfit = 0
 changes = []
 
 with open(csvpath) as csvfile:
@@ -27,34 +24,34 @@ with open(csvpath) as csvfile:
 
     # Read the header row first
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
     for row in csvreader:
+        # Print all rows of the csv
         #print(row)
-        #Set the current row to the 
+        
+        # Set the current row to the 
         currentAmount = int(row[1])
-        #Add current row to total
-        total += currentAmount
-        #Check for largest Profit
-        if currentAmount > bigProfit:
-            bigProfit = currentAmount
-        #Check for largest Loss
-        if currentAmount < bigLoss:
-            bigLoss = currentAmount
-        #Add the change from the previous row amount to the list
+        # Add current row to netProfit
+        netProfit += currentAmount
+        # Add the change from the previous row amount to the list of changes from month to month
+        # previousAmount will not exist while we are on the first row(and the value in the first month isn't a change it's a starting value)
         if 'previousAmount' in globals():
-            changes.append(currentAmount - previousAmount)        
-        #Increment the number of months(rows)
+            changes.append([row[0], currentAmount - previousAmount])        
+        # Increment the number of months(rows)
         numMonths += 1
-        #Set the previousAmount to the current for the next row
+        # Set the previousAmount to the current right before moving to the next row
         previousAmount = currentAmount
 
-# Remove the first item of the changes list
-# (The first value in the csv is a starting value not an increase from 0)        
-#changes.pop(0)
-#Output
+    for row in changes:
+        # Print all rows of the csv
+        print(row)
+
+# Calculate avg change/largest growth/largest loss
+#for row in changes:
+    
+# Output
 print("Financial Analysis\n----------------------")
 print("Total Months:", numMonths)
-print(f"Total: ${total}")
-print(f"Average Change: ${mean(changes)}")
+print(f"Total: ${netProfit}")
