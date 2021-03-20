996945ee0097b7da18097549fc84724ef5bd4144
Henry Poe <hpoe111@gmail.com>
2021-03-20 13:01:57 -0400

    experimenting with solutions for the avg change

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,6 +6,8 @@
 import os
 # Module for reading CSV files
 import csv
+# Module for mean
+from statistics import mean
 
 # Create the path for our data set
 csvpath = os.path.join('Resources', 'budget_data.csv')
@@ -13,7 +15,7 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 # Initialize some variable with their starting values
 numMonths = 0
 total = 0
-previousAmount = 0
+#previousAmount = 0
 bigProfit = 0
 bigLoss = 0
 changes = []
@@ -29,7 +31,7 @@ with open(csvpath) as csvfile:
 
     # Read each row of data after the header
     for row in csvreader:
-        print(row)
+        #print(row)
         #Set the current row to the 
         currentAmount = int(row[1])
         #Add current row to total
@@ -41,12 +43,18 @@ with open(csvpath) as csvfile:
         if currentAmount < bigLoss:
             bigLoss = currentAmount
         #Add the change from the previous row amount to the list
-        changes.append(currentAmount - previousAmount)        
+        if 'previousAmount' in globals():
+            changes.append(currentAmount - previousAmount)        
         #Increment the number of months(rows)
         numMonths += 1
         #Set the previousAmount to the current for the next row
         previousAmount = currentAmount
-        
+
+# Remove the first item of the changes list
+# (The first value in the csv is a starting value not an increase from 0)        
+#changes.pop(0)
 #Output
 print("Financial Analysis\n----------------------")
 print("Total Months:", numMonths)
+print(f"Total: ${total}")
+print(f"Average Change: ${mean(changes)}")
